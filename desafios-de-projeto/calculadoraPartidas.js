825e643ddbe53d2e5c9bdba824328baed92c553c
htonioni <htonioni@outlook.com>
2023-12-11 11:01:53 -0300

    Added rank calculator project

--- desafios-de-projeto/calculadoraPartidas.js
+++ desafios-de-projeto/calculadoraPartidas.js
@@ -1,8 +1,44 @@
-let eu = 'Hugo Sant Marrocos'
-let primeiroNome = getFirstName(eu, " ")
-console.log(primeiroNome);
+let vitorias = 120
+let derrotas = 20
+let rank = 0
+let patente = ""
 
-function getFirstName (entrada, caracter){
-    // quebra a variavel em " " e seleciona o indice 0 
-    return entrada.split(caracter)[0]
+function calculaRank(victories, defeats) {
+    return rank = victories - defeats
 }
+
+calculaRank(vitorias, derrotas)
+ 
+console.log(rank);
+
+switch (true) {
+    case rank <= 10:
+        patente = "Ferro"
+        break;
+
+    case rank <= 20:
+        patente = "Bronze"
+        break;
+
+    case rank <= 50:
+        patente = "Prata"
+        break; 
+
+    case rank <= 80:
+        patente = "Ouro"
+        break;  
+    
+    case rank <= 90:
+        patente = "Platina"
+        break;  
+
+    case rank <= 100:
+        patente = "Ascendente"
+        break;  
+
+    case rank >= 101:
+        patente = "Imortal"
+        break;  
+}
+
+console.log(`O heroi tem rank: ${rank} e está no nivel ${patente}`);
